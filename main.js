f0eaafbe9fb89f3d9b7cd0dd9e44128f6cb35dfb
Apoorva584 <pavuluriapoorva786@gmail.com>
2018-07-07 14:59:01 +0530

    addingfile

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ getfile("data.json",function(text){
 let data = JSON.parse(text);
 console.log(data);
 career(data.career);
-education(data,education);
+education(data.education);
+skills(data.skills);
 }
 )
 var childtwo=document.querySelector(".childtwo");
@@ -37,6 +38,23 @@ childtwo.appendChild(careerHr);
 var edutable=document.createElement("table");
 edutable.border="1";
 var tr1="<tr><td>degree</td><td>Institute</td><td>Data</td></tr>";
-edutable.innerHTML=tr1;
+//edutable.innerHTML=tr1;
+var tr2="";
+// console.log(edu.length);
+for(var i=0; i<edu.length; i++){
+  console.log(edu.length);
+tr2+="<tr><td>"+edu[i].degree+"</td><td>"+edu[i].institute+"</td><td>"+edu[i].data+"</td></tr>";
+console.log(tr2);
+}
+edutable.innerHTML=tr1+tr2;
 childtwo.appendChild(edutable);
+}
+function skills(skil){
+  var ul=document.createElement("ul");
+  childtwo.appendChild(ul);
+  for(var i=0; i<skil.length; i++){
+    var li=document.createElement("li");
+    li.textContent=skil[i].title+":"+skil[i].content;
+    ul.appendChild(li);
+  }
 }
