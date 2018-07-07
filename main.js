caf2212e66b6180e6ea6d10330d7de28bbfd43d2
Apoorva584 <pavuluriapoorva786@gmail.com>
2018-07-07 13:34:57 +0530

    addingfile

--- main.js
+++ main.js
@@ -12,5 +12,31 @@ xhr.send(null);
 getfile("data.json",function(text){
 let data = JSON.parse(text);
 console.log(data);
+career(data.career);
+education(data,education);
 }
 )
+var childtwo=document.querySelector(".childtwo");
+function career(careerInfo){
+  console.log(careerInfo);
+  var careerHeading=document.createElement("h3");
+  childtwo.appendChild(careerHeading);
+  careerHeading.textContent="career objective";
+  var careerHr=document.createElement("Hr");
+  childtwo.appendChild(careerHr);
+  var ch=document.createElement("p");
+  ch.textContent=careerInfo.info;
+  childtwo.appendChild(ch);
+}
+function education(edu){
+var eduHeading=document.createElement("h3");
+eduHeading.textContent="Educational Qualifications";
+childtwo.appendChild(eduHeading);
+var careerHr=document.createElement("Hr");
+childtwo.appendChild(careerHr);
+var edutable=document.createElement("table");
+edutable.border="1";
+var tr1="<tr><td>degree</td><td>Institute</td><td>Data</td></tr>";
+edutable.innerHTML=tr1;
+childtwo.appendChild(edutable);
+}
